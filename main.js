39f27d0c82dfecb4997405a5d13dbe27cf2f41cc
Caseywalker <casey.walker118@gmail.com>
2021-01-23 10:57:05 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('CONNECTED');
+// console.log('CONNECTED');
+
+const theOne = document.getElementById('main-btn');
+console.log(theOne);
+
+const button = document.querySelector('#main-btn');
+console.log(button)
