2a2d7651eda59b7d6cec4abcb94105bf0a30a916
Caseywalker <casey.walker118@gmail.com>
2021-01-23 11:08:52 -0600

    working with callbacks

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('CONNECTED');
+// console.log('CONNECTED');
+
+const function1 = (user) => {
+  return `Hello, ${user}`;
+};
+
+const function2 = function(user) {
+  return `Goodbye, ${user}`;
+};
+
+const function3 = function(user, cb) {
+  return cb(user);
+};
+
+console.log(function3('Casey', function2));
